d604bc5315258f6dc9f1a3226a7b505991f252d1
Twinpeakss <49532214+Twinpeakss@users.noreply.github.com>
2019-11-03 17:38:20 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Perceptron_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(735, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseWaitCursor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(735, 308);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(298, 241);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.UseWaitCursor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(921, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseWaitCursor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 561);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using OxyPlot.WindowsForms;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace Perceptron_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+       // Если у!=е в одной строке то значения w1,w2,Т считать по формуле ниже
+        // w1(t+1)=w1(t)-alpha*x1*(y-e)
+        // w2(t+1)=w2(t)-alpha*x2*(y-e)
+        // T(t+1)=T(t)+alpha *(y-e)
+        // else  
+        // S=w1*x1+w2*x2-T
+
+        //Нам нужно считать сумму S  на первом шаге и дальше проверить ровно ли значение designed otput значению real otput
+        //Если равно то используем значения весов  в слудющей линии и рассчитываем по той же формуле
+        //Иначе мы должны посчитать значения весов по формуле
+
+       
+
+
+        int[,] inputSamples = new int[,]{
+                                        {1,1 },
+                                        {-1,1 },
+                                        {-1,-1 },
+                                        {1,-1 }};
+
+        int[] designed_otput = new int[] { 1, 1, 0, 0 };
+
+        double alpha = 0.1;
+        int y = 0;
+        Random rn = new Random();
+
+        double[] Results;
+        int global_epoch = 1;
+        double w1;
+        double w2;
+        double Threshold;
+
+        double[,] tab = new double[4, 2];
+        double x1, x2, k;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Results = new double[4];
+            double Threshold = rn.NextDouble() * 2 - 1.0;
+            double w1 = rn.NextDouble() * 2 - 1.0;
+            double w2 = rn.NextDouble() * 2 - 1.0;
+            int condition_counter = 0;
+            int equals_counter = 0;
+
+            while (equals_counter != 4 && condition_counter != 4)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    double S = w1 * inputSamples[i, 0] + w2 * inputSamples[i, 1] - Threshold;
+                    if (S > 0)
+                    {
+                        y = 1;
+                    }
+                    else
+                    {
+                        y = 0;
+                    }
+
+                    Results[i] = y;
+                    if (y != designed_otput[i])
+                    {
+                        w1 = w1 - alpha * inputSamples[i, 0] * (y - designed_otput[i]);
+                        w2 = w2 - alpha * inputSamples[i, 1] * (y - designed_otput[i]);
+                        Threshold = Threshold + alpha * (y - designed_otput[i]);
+                    }
+                    else
+                    {
+                        condition_counter++;
+                    }
+
+                    equals_counter++;
+
+                    if (equals_counter == 4 && condition_counter != 4)
+                    {
+                        equals_counter = 0;
+                        condition_counter = 0;
+                    }
+                    global_epoch++;
+                }
+
+                //Console.WriteLine("w1=" + w1);
+                //Console.WriteLine("w2=" + w2);
+                //Console.WriteLine("Threshold=" + Threshold);
+            }
+            bool flag = true;
+            for (int i = 0; i < 4; i++)
+            {
+                if (Results[i] != designed_otput[i])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            textBox1.Text = "w1=" + w1 + Environment.NewLine;
+            textBox1.Text += "w2=" + w2 + Environment.NewLine;
+            textBox1.Text += "Threshold=" + Threshold + Environment.NewLine;
+            textBox1.Text += "flag:" + flag + Environment.NewLine;
+            textBox1.Text += "Epoch:" + global_epoch + Environment.NewLine;
+            //////////////////////////////////////////////////////////////////
+
+            textBox1.Text += "x1:" + x1 + Environment.NewLine;
+            textBox1.Text += "x2=" + x2 + Environment.NewLine;
+
+
+
+            //x2 = Threshold / w2; //dla 0
+
+            //x1 = Threshold / w1;
+
+
+            int j = -100;    
+            tab[0, 0] = j;
+            tab[0, 1] = (Threshold / w2) - ((j * w1 / w2));//x1=j
+
+            j = 100;
+
+            tab[1, 0] = j;
+            tab[1, 1] = (Threshold / w2) - ((j * w1 / w2));//x1=j
+
+
+
+
+
+
+
+
+            
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PlotView pv = new PlotView();
+            pv.Location = new Point(0, 0);
+            pv.Size = new Size(500, 400);
+            
+            
+            this.Controls.Add(pv);
+
+            pv.Model = new PlotModel { Title = "Perceptron's separation line" };
+            FunctionSeries fs = new FunctionSeries();
+            //fs.Points.Add(new OxyPlot.DataPoint(x1, 0));
+            //fs.Points.Add(new OxyPlot.DataPoint(0, x2)); 
+            
+            for(int i = 0; i < 2; i++) 
+            {              
+                fs.Points.Add(new OxyPlot.DataPoint(tab[i,0], tab[i, 1]));              
+            }
+
+            //OxyPlot.Axes.LinearAxis fAxis = new LinearAxis(OxyPlot.Axes.AxisPosition.Left, 0.0, 15.0);
+            //OxyPlot.Axes.LinearAxis aAxis = new  LinearAxis(OxyPlot.Axes.AxisPosition.Right, 0.0, 20.0);
+            pv.Model.Series.Add(fs);
+
+            ScatterSeries sc = new ScatterSeries();          
+            sc.Points.Add(new ScatterPoint(inputSamples[0, 0], inputSamples[0, 1]));
+            sc.Points.Add(new ScatterPoint(inputSamples[1, 0], inputSamples[1, 1]));
+            sc.Points.Add(new ScatterPoint(inputSamples[2, 0], inputSamples[2, 1]));
+            sc.Points.Add(new ScatterPoint(inputSamples[3, 0], inputSamples[3, 1]));
+           
+            pv.Model.Series.Add(sc);
+            
+        }
+
+       
+
+    }
+}
